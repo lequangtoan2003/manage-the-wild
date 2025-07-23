7db4cef968ed831f29c6adcab44a7145c2035b65
unknown <toan9370@donga.edu.vn>
2025-07-24 01:33:04 +0700

    feature/lequangtoan(form-error-and-upload-image)

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,16 +1,36 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useForm } from 'react-hook-form';
+import { createCabin } from '../../services/apiCabins';
+import toast from 'react-hot-toast';
+import Error from '../../ui/Error';
 
 export default function CreateCabinForm() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset, getValues, formState } = useForm();
+  const { errors } = formState;
+  const queryClient = useQueryClient();
+
+  const { mutate, isLoading: isCreating } = useMutation({
+    mutationFn: createCabin,
+    onSuccess: () => {
+      toast.success('Cabin created successfully');
+      queryClient.invalidateQueries({ queryKey: ['cabins'] });
+      reset();
+    },
+    onError: (err) => {
+      toast.error(err.message || 'Error creating cabin');
+    },
+  });
 
   function onSubmit(data) {
-    console.log('Cabin data submitted:', data);
-    // Ở đây bạn có thể gửi dữ liệu đến API để tạo cabin mới
+    mutate({ ...data, image: data.image[0] });
+  }
+  function onError(errors) {
+    console.log('Form errors:', errors);
   }
 
   return (
     <form
-      onSubmit={handleSubmit(onSubmit)}
+      onSubmit={handleSubmit(onSubmit, onError)}
       className="mt-2 flex flex-col gap-4 rounded-lg bg-grey-0 p-8 shadow-md"
     >
       <div className="grid grid-cols-[20rem_1fr_1.2fr] gap-4">
@@ -19,61 +39,91 @@ export default function CreateCabinForm() {
           className="rounded-lg border-2 outline-none"
           type="text"
           id="name"
-          {...register('name')}
+          {...register('name', { required: 'This field is required' })}
         />
-        <div className=""></div>
+        {errors?.name?.message && <Error>{errors.name.message}</Error>}
       </div>
       <div className="grid grid-cols-[20rem_1fr_1.2fr] gap-4">
         <label>Maximum Capacity</label>
         <input
           className="rounded-lg border-2 outline-none"
           type="number"
+          disabled={isCreating}
           id="maxCapacity"
-          {...register('maxCapacity')}
+          {...register('maxCapacity', {
+            required: 'This field is required',
+            min: {
+              value: 1,
+              message: 'Capacity must be at least 1',
+            },
+          })}
         />
-        <div className=""></div>
+        {errors?.maxCapacity?.message && (
+          <Error>{errors.maxCapacity.message}</Error>
+        )}
       </div>
       <div className="grid grid-cols-[20rem_1fr_1.2fr] gap-4">
         <label>Regular price</label>
         <input
           className="rounded-lg border-2 outline-none"
           type="number"
+          disabled={isCreating}
           id="regularPrice"
-          {...register('regularPrice')}
+          {...register('regularPrice', {
+            required: 'This field is required',
+            min: {
+              value: 1,
+              message: 'Capacity must be at least 1',
+            },
+          })}
         />
-        <div className=""></div>
+        {errors?.regularPrice?.message && (
+          <Error>{errors.regularPrice.message}</Error>
+        )}
       </div>
       <div className="grid grid-cols-[20rem_1fr_1.2fr] gap-4">
         <label>Discount</label>
         <input
           className="rounded-lg border-2 outline-none"
           type="number"
+          disabled={isCreating}
           id="discount"
           defaultValue={0}
-          {...register('discount')}
+          {...register('discount', {
+            required: 'This field is required',
+            validate: (value) =>
+              value <= getValues().regularPrice ||
+              'Discount should be less than regular price',
+          })}
         />
-        <div className=""></div>
+        {errors?.discount?.message && <Error>{errors.discount.message}</Error>}
       </div>
       <div className="grid grid-cols-[20rem_1fr_1.2fr] gap-4">
         <label>Description for website</label>
         <input
           className="rounded-lg border-2 outline-none"
           type="text"
+          disabled={isCreating}
           id="description"
-          {...register('description')}
+          {...register('description', { required: 'This field is required' })}
         />
-        <div className=""></div>
+        {errors?.description?.message && (
+          <Error>{errors.description.message}</Error>
+        )}
       </div>
       <div className="grid grid-cols-[20rem_1fr_1.2fr] gap-4">
         <label>Cabin photo</label>
-        <input
-          className="rounded-lg border-2 outline-none"
-          type="file" // Đổi thành type="file" để upload ảnh
-          accept="image/*"
-          id="image"
-          {...register('image')} // Đăng ký input này với react-hook-form
-        />
-        <div className=""></div>
+        <div className="">
+          <input
+            className="max-w-[296px] rounded-lg border-2 outline-none"
+            type="file" // Đổi thành type="file" để upload ảnh
+            disabled={isCreating}
+            accept="image/*"
+            id="image"
+            {...register('image')} // Đăng ký input này với react-hook-form
+          />
+        </div>
+        {errors?.image?.message && <Error>{errors.image.message}</Error>}
       </div>
       <div className="grid grid-cols-[24rem_1fr_1.2fr] gap-4">
         <div className=""></div>
@@ -88,6 +138,7 @@ export default function CreateCabinForm() {
           <button
             type="submit"
             className="rounded-lg bg-blue-500 px-4 py-2 text-white"
+            disabled={isCreating}
           >
             Create
           </button>
--- src/services/apiCabins.jsx
+++ src/services/apiCabins.jsx
@@ -1,4 +1,4 @@
-import supabase from './supabase';
+import supabase, { supabaseUrl } from './supabase';
 
 export async function getCabins() {
   const { data, error } = await supabase.from('cabins').select('*');
@@ -8,6 +8,38 @@ export async function getCabins() {
   }
   return data;
 }
+
+export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    '/',
+    ''
+  );
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+  //https://lecnynvyzqecwagnlftj.supabase.co/storage/v1/object/public/cabin-images//bg.jpg
+  //create cabin
+  const { data, error } = await supabase
+    .from('cabins')
+    .insert([{ ...newCabin, image: imagePath }])
+    .select();
+
+  if (error) {
+    console.error('Error creating cabin:', error);
+    throw new Error('Cabin could not be created');
+  }
+
+  //upload image
+  const { error: storageError } = await supabase.storage
+    .from('cabin-images')
+    .upload(imageName, newCabin.image);
+  //uploading error
+  if (storageError) {
+    await supabase.from('cabins').delete().eq('id', data.id);
+    console.error('Error uploading cabin image:', storageError);
+    throw new Error('Cabin image could not be uploaded');
+  }
+  return data;
+}
+
 export async function deleteCabin(id) {
   const { data, error } = await supabase.from('cabins').delete().eq('id', id);
   if (error) {
