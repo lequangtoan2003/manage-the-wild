9fde9271953e0b4caccf1efd20d63c22f4fb5b24
unknown <toan9370@donga.edu.vn>
2025-08-04 14:52:45 +0700

    feature/lequangtoan(authentication-authorization)

---
+++ src/services/apiAuth.js
@@ -0,0 +1,31 @@
+import supabase from './supabase';
+
+export async function login({ email, password }) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (error) throw new Error(error.message);
+  console.log(data);
+  return data;
+}
+
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession();
+  console.log('Session:', session);
+
+  if (!session.session) {
+    console.log('No active session found');
+    return null;
+  }
+
+  const { data, error } = await supabase.auth.getUser();
+  console.log('User data:', data?.user);
+
+  if (error) {
+    console.error('Get user error:', error);
+    throw new Error(error.message);
+  }
+
+  return data?.user;
+}
